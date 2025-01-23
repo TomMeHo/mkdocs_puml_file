45b78c5063e021ac7aaa05bdecc20433a5abd17d
Thomas Meder <thomas.meder@sap.com>
2025-01-23 15:51:18 +0000

    test env added

---
+++ example/docs/index.md
@@ -0,0 +1,5 @@
+# Test Page
+
+Here, a PUML file should be embedded:
+
+![test puml file](diagram.puml)
--- mkdocs_puml_file/plugin.py
+++ mkdocs_puml_file/plugin.py
@@ -1,5 +1,24 @@
+import os
+import re
+import string
+import logging
+import mkdocs
+from mkdocs.plugins import BasePlugin
+
+
 # ------------------------
 # Plugin
 # ------------------------
+
+REPLACEMENT_PATTERN = r'!\[(.*?)\]\((.*?.puml)\)'
+
 class PlantUmlFilePlugin(BasePlugin):
-    pass
+    """
+    Plugin that allows to embed PlantUML files into your markdown documents.
+    """
+    config_scheme = (
+        (
+            "file_extension",
+            mkdocs.config.config_options.Type(str, default=".puml"),
+        ),
+    )
