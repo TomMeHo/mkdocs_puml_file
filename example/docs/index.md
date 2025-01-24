990493ef933c44acbfc2e4001581d87b9ce6fa57
Thomas Meder <thomas.meder@sap.com>
2025-01-24 16:39:19 +0000

    adding setup.py

--- example/docs/index.md
+++ example/docs/index.md
@@ -3,3 +3,8 @@
 Here, a PUML file should be embedded:
 
 ![test puml file](diagram.puml)
+
+Text after the embedded fence.
+
+Second embedding:
+![test puml file](diagram.puml)
--- mkdocs_puml_file/plugin.py
+++ mkdocs_puml_file/plugin.py
@@ -1,24 +1,35 @@
-import os
 import re
-import string
-import logging
 import mkdocs
 from mkdocs.plugins import BasePlugin
+from mkdocs.structure.pages import Page
+from mkdocs.config.defaults import MkDocsConfig
+from mkdocs.structure.files import Files
 
 
 # ------------------------
 # Plugin
 # ------------------------
 
-REPLACEMENT_PATTERN = r'!\[(.*?)\]\((.*?.puml)\)'
+SEARCH_PATTERN = r'!\[(.*?)\]\((.*?.puml)\)'
+REPLACEMENT_PATTERN = r'```puml\n<$2.puml>\n```'
 
 class PlantUmlFilePlugin(BasePlugin):
     """
     Plugin that allows to embed PlantUML files into your markdown documents.
     """
-    config_scheme = (
+    config_scheme = (   
         (
             "file_extension",
             mkdocs.config.config_options.Type(str, default=".puml"),
         ),
     )
+
+    def __init__(self):
+        pass
+        #self.log = logging.getLogger("mkdocs.plugins.diagrams")
+        #self.pool = None
+
+
+    def on_pre_page( self, page: Page, /, *, config: MkDocsConfig, files: Files ):
+        page.markdown = re.sub( SEARCH_PATTERN, REPLACEMENT_PATTERN, page.markdown )
+        return page
